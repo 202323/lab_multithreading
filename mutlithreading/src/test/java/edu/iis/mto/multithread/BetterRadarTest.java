d7b545b48a56eb194b02693c7d0a10bf9fbedf8d
StrollingOx <StrollingOx@github.com>
2019-07-01 18:31:19 +0200

    Added RepeatRule class and two tests that follows repeat rule

--- mutlithreading/src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
@@ -30,4 +30,24 @@ public class BetterRadarTest {
         verify(patriotBattery, times(2)).launchPatriot();
     }
 
+    @Test
+    @RepeatRule.Repeat(times = 5)
+    public void launchPatriotOnceFromThisThreadWithFiveRepeats() {
+        launchSAMs = new LaunchSAMs(ExecutionLocation.FROM_HERE);
+        betterRadar = new BetterRadar(patriotBattery, launchSAMs, 1);
+        betterRadar.notice(new Scud());
+
+        verify(patriotBattery, times(1)).launchPatriot();
+    }
+
+    @Test
+    @RepeatRule.Repeat(times = 10)
+    public void launchPatriotOnceFromAnotherThreadWithTenRepeats() {
+        launchSAMs = new LaunchSAMs(ExecutionLocation.FROM_HERE);
+        betterRadar = new BetterRadar(patriotBattery, launchSAMs, 1);
+        betterRadar.notice(new Scud());
+
+        verify(patriotBattery, times(1)).launchPatriot();
+    }
+
 }
